6ba1414d192b13d5df0c18d2b52e7de1de7ebb4b
alexey podvoyskiy <fell978@yandex.ru>
2016-07-24 13:50:09 +0400

    Update return_address.c

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -59,9 +59,9 @@ void *return_address(unsigned int level)
 
 #else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
 
-#if defined(CONFIG_ARM_UNWIND)
+/* #if defined(CONFIG_ARM_UNWIND)
 #warning "TODO: return_address should use unwind tables"
-#endif
+#endif */
 
 void *return_address(unsigned int level)
 {
